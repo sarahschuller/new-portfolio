7e9e470617db3915a1008e1a92708b16f7e24a35
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2023-03-26 15:44:02 -0400

    create new react app and add router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from 'react-router-dom';
+
+// Scroll to the top of the page when changing routes
+// https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0,0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+        <ScrollToTop />
+        <Switch>
+          <Route path="/about">
+            <h1>About Me</h1>
+          </Route>
+          <Route path="/work">
+            <h1>My Work111</h1>
+          </Route>
+          <Route path="/contact">
+            <h1>Contact Me</h1>
+          </Route>
+          <Route path="/">
+            <h1>Main</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
