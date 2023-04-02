590f6af931792e11a31c0302f6dcfa0811cf5256
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2023-04-02 13:23:21 -0400

    create outline for project data array

---
+++ src/data/projects.js
@@ -0,0 +1,12 @@
+const projects = {
+    project1: {
+        name: "Evil Genius",
+        image: "",
+        desc: "",
+        gitlink: "",
+        live: "",
+        casestudy: "",
+    }
+}
+
+export default projects;
